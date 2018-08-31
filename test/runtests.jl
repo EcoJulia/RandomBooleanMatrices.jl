14c292cc7050e7fefeb7756d45e19d81d1faa9e6
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2018-08-31 10:29:23 +0200

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,14 @@ Random.seed!(1337)
 
 @testset "curveball" begin
     m = sprand(Bool, 8, 6, 0.2)
+    m_old = copy(m)
     rsm = sum(m, dims = 1)
     csm = sum(m, dims = 2)
     randomize_matrix!(m, method = curveball)
 
     @test rsm == sum(m, dims = 1)
     @test csm == sum(m, dims = 2)
+    @test m_old != m
 
     m2 = rand(0:1, 6, 5)
     rsm = sum(m2, dims = 1)
